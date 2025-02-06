344c74ab9acd3924f9a0d2af9a36c34fd03e38d4
basarat <basaratali@gmail.com>
2025-02-06 19:00:48 +1100

    reset

--- lessons/fonts_next_local/src/app/page.tsx
+++ lessons/fonts_next_local/src/app/page.tsx
@@ -1,14 +1,8 @@
 import { Lorem } from "./lorem";
-import { Roboto } from "next/font/google";
-
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-});
 
 export default function Home() {
   return (
-    <div className={roboto.className}>
+    <div>
       <Lorem />
     </div>
   );
