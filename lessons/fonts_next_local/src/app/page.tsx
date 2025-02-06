98eadac5517220463210146fba1749825cf024f3
basarat <basaratali@gmail.com>
2025-02-06 19:06:00 +1100

    local

--- lessons/fonts_next_local/src/app/page.tsx
+++ lessons/fonts_next_local/src/app/page.tsx
@@ -1,8 +1,13 @@
 import { Lorem } from "./lorem";
+import localFont from "next/font/local";
+
+const SuperShiny = localFont({
+  src: "./fonts/SuperShiny.woff2",
+});
 
 export default function Home() {
   return (
-    <div>
+    <div className={SuperShiny.className}>
       <Lorem />
     </div>
   );
