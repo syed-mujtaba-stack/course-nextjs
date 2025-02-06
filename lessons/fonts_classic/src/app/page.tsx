52018eb2e830249d31086cd36eb74b1623b6d131
basarat <basaratali@gmail.com>
2025-02-06 18:38:21 +1100

    classic demo done

--- lessons/fonts_classic/src/app/page.tsx
+++ lessons/fonts_classic/src/app/page.tsx
@@ -1,12 +1,9 @@
-import Head from "next/head";
 import { Lorem } from "./lorem";
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <Lorem />
-      </div>
-    </>
+    <div style={{ fontFamily: "Roboto, serif" }}>
+      <Lorem />
+    </div>
   );
 }
