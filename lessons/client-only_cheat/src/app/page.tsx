0fadaf7684b30aca4636931295d48d80fe2730aa
basarat <basaratali@gmail.com>
2025-02-06 16:22:05 +1100

    use client required

--- lessons/client-only_cheat/src/app/page.tsx
+++ lessons/client-only_cheat/src/app/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import dynamic from "next/dynamic";
 
-const ClientOnlyRoot = dynamic(() => import("./ClientOnlyRoot"), { ssr: false });
+const ClientOnlyRoot = dynamic(() => import("./ClientOnlyRoot"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
--- lessons/client-only_motivation/src/app/page.tsx
+++ lessons/client-only_motivation/src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import dynamic from "next/dynamic";
 
 const WindowWidth = dynamic(() => import("./WindowWidth"), { ssr: false });
