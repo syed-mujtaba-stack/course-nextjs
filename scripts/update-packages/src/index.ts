785840e9b168144ccf11082e7d98368953633415
basarat <basaratali@gmail.com>
2025-02-06 15:34:34 +1100

    better

--- scripts/update-packages/src/index.ts
+++ scripts/update-packages/src/index.ts
@@ -2,7 +2,7 @@ import path from "path";
 import { promises as fs } from "fs";
 import { fileURLToPath } from "url";
 import { execa } from "execa";
-import { deps } from "./thingsToUpdate.js";
+import { deps } from "./thingsToUpdate.ts";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
