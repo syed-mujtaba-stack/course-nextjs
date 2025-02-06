68c0121c928dced17f6c6c55886123d7467de0a7
basarat <basaratali@gmail.com>
2025-02-06 15:38:36 +1100

    better

--- scripts/update-packages/src/index.ts
+++ scripts/update-packages/src/index.ts
@@ -17,46 +17,60 @@ async function getLessonDirectories() {
   return lessonDirectories;
 }
 
+function getLesssonDirPath(lessonDir: string) {
+  return path.join(lessonsRootDir, lessonDir);
+}
+
+async function updatePackageJson(lessonDir: string) {
+  console.log(
+    `
+------------------------------
+${lessonDir}: Updating Packages...
+------------------------------
+    `.trim()
+  );
+  const lessonDirPath = getLesssonDirPath(lessonDir);
+
+  /**
+   * We update the package.json file manually to ensure that things like `^` are preserved
+   */
+  const lessonPackageJsonPath = path.join(lessonDirPath, "package.json");
+  const lessonPackage = JSON.parse(
+    await fs.readFile(lessonPackageJsonPath, "utf-8")
+  );
+  const newlessonPackage = {
+    ...lessonPackage,
+    dependencies: {
+      ...lessonPackage.dependencies,
+      ...deps.dependencies,
+    },
+    devDependencies: {
+      ...lessonPackage.devDependencies,
+      ...deps.devDependencies,
+    },
+  };
+  await fs.writeFile(
+    lessonPackageJsonPath,
+    JSON.stringify(newlessonPackage, null, 2) + "\n"
+  );
+
+  /**
+   * Install the packages
+   */
+  await execa("npm", ["install"], {
+    cwd: lessonDirPath,
+    stdio: "inherit",
+  });
+}
+
 async function main() {
   const lessonDirectories = await getLessonDirectories();
   console.log(lessonDirectories);
 
   for (const lessonDir of lessonDirectories) {
-    const lessonDirPath = path.join(lessonsRootDir, lessonDir);
+    await updatePackageJson(lessonDir);
 
     try {
-      console.log(`Updating ${lessonDir}...`);
-
-      /**
-       * We update the package.json file manually to ensure that things like `^` are preserved
-       */
-      const lessonPackageJsonPath = path.join(lessonDirPath, "package.json");
-      const lessonPackage = JSON.parse(
-        await fs.readFile(lessonPackageJsonPath, "utf-8")
-      );
-      const newlessonPackage = {
-        ...lessonPackage,
-        dependencies: {
-          ...lessonPackage.dependencies,
-          ...deps.dependencies,
-        },
-        devDependencies: {
-          ...lessonPackage.devDependencies,
-          ...deps.devDependencies,
-        },
-      };
-      await fs.writeFile(
-        lessonPackageJsonPath,
-        JSON.stringify(newlessonPackage, null, 2) + "\n"
-      );
-
-      /**
-       * Install the packages
-       */
-      await execa("npm", ["install"], {
-        cwd: lessonDirPath,
-        stdio: "inherit",
-      });
     } catch (error) {
       console.error(`Failed to update ${lessonDir}: ${error}`);
     }
