37ffbe7e0cd753fcddbc1e49ffd09ae05de0b969
basarat <basaratali@gmail.com>
2025-02-13 14:16:31 +1100

    example

---
+++ lessons/link_a_bad/src/app/alpha/page.tsx
@@ -0,0 +1,7 @@
+export default function Alpha() {
+  return (
+    <div>
+      <a href="/beta">Go to Beta</a>
+    </div>
+  );
+}
---
+++ lessons/link_a_bad/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "BooleanArt NextJS App",
+  description: "Sample App for the BooleanArt NextJS Course",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  console.log("RootLayout");
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
