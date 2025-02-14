ea1f52e8372edb3a01a70e523bc5e5e7d27cff7a
basarat <basaratali@gmail.com>
2025-02-14 19:14:44 +1100

    better

--- scripts/update-lessons/src/utils/cleanModules.ts
+++ scripts/update-lessons/src/utils/cleanModules.ts
@@ -4,39 +4,41 @@ import path from "path";
 import { logDone, logSection } from "./logSection.ts";
 import { getLesssonDirPath } from "../index.ts";
 
-async function rm(path: string) {
+async function rm(path: string, silent = false) {
   try {
     await fs.rm(path, { recursive: true });
   } catch (error: any) {
     if ((error as NodeJS.ErrnoException).code === "ENOENT") {
-      console.log(`Path not found ❌`);
-      console.log(`Path: ${path}`);
+      if (!silent) {
+        console.log(`Path not found ❌`);
+        console.log(`Path: ${path}`);
+      }
     } else {
       throw error;
     }
   }
 }
 
-async function cleanNodeModules(dir: string) {
+async function cleanNodeModules(dir: string, silent: boolean) {
   const nodeModules = path.resolve(dir, "./node_modules");
-  await rm(nodeModules);
+  await rm(nodeModules, silent);
 }
 
-async function cleanPackageLock(dir: string) {
+async function cleanPackageLock(dir: string, silent: boolean) {
   const packageLock = path.resolve(dir, "./package-lock.json");
-  await rm(packageLock);
+  await rm(packageLock, silent);
 }
 
 export async function cleanModulesRoot() {
   logSection("Cleaning root node_modules / package-lock.json");
-  await cleanNodeModules(rootDir);
-  await cleanPackageLock(rootDir);
+  await cleanNodeModules(rootDir, false);
+  await cleanPackageLock(rootDir, false);
   logDone();
 }
 
 export async function cleanModulesLesson(lessonDir: string) {
   logSection(`${lessonDir}: Cleaning node_modules`);
   const lessonDirPath = getLesssonDirPath(lessonDir);
-  await cleanNodeModules(lessonDirPath);
+  await cleanNodeModules(lessonDirPath, true);
   logDone();
 }
