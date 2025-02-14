89923a4728c079085b2a0aeb50a188b63d676c44
basarat <basaratali@gmail.com>
2025-02-14 19:00:28 +1100

    better

--- scripts/update-lessons/src/utils/cleanModules.ts
+++ scripts/update-lessons/src/utils/cleanModules.ts
@@ -3,19 +3,23 @@ import { rootDir } from "./lessonsRootDir.ts";
 import path from "path";
 import { logDone, logSection } from "./logSection.ts";
 
-export async function cleanModulesRoot() {
-  logSection("Cleaning root node_modules");
-  const rootNodeModules = path.resolve(rootDir, "./node_modules");
+async function cleanModules(dir: string) {
+  const nodeModules = path.resolve(dir, "./node_modules");
   try {
-    await fs.rm(rootNodeModules, { recursive: true });
+    await fs.rm(nodeModules, { recursive: true });
   } catch (error: any) {
     console.log('error', error.code);
     if ((error as NodeJS.ErrnoException).code === "ENOENT") {
-      console.log(`Root node_modules not found`);
-      console.log(`Path: ${rootNodeModules}`);
+      console.log(`node_modules not found`);
+      console.log(`Path: ${nodeModules}`);
     } else {
       throw error;
     }
   }
+}
+
+export async function cleanModulesRoot() {
+  logSection("Cleaning root node_modules");
+  await cleanModules(rootDir);
   logDone();
 }
