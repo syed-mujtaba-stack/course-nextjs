a84dc86adeff55cf1986ef558abf622d951620cc
basarat <basaratali@gmail.com>
2025-02-17 15:32:11 +1100

    better

--- lessons/link-prefetch_core/src/app/example/[id]/page.tsx
+++ lessons/link-prefetch_core/src/app/example/[id]/page.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  return <div>Example: {id}</div>;
+  return (
+    <>
+      <Link href="/">← Home</Link>
+      <div>Example: {id}</div>
+    </>
+  );
 }
