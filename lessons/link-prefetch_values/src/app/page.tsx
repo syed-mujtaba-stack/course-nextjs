2de63888b5215896d172622d73139d1b3342c9fb
basarat <basaratali@gmail.com>
2025-02-17 17:17:28 +1100

    better

--- lessons/link-prefetch_values/src/app/page.tsx
+++ lessons/link-prefetch_values/src/app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Link href="/value/true">true</Link>
       <Link href="/value/false">false</Link>
+      <Link href="/value/true">true</Link>
       <Link href="/value/null">null</Link>
     </div>
   )
