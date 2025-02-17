4d68010a0cd95a5c98356d063a285763633f5a1f
basarat <basaratali@gmail.com>
2025-02-17 17:37:02 +1100

    better

--- lessons/link-prefetch_values/src/app/dynamic/page.tsx
+++ lessons/link-prefetch_values/src/app/dynamic/page.tsx
@@ -1,6 +1,7 @@
 export const dynamic = "force-dynamic";
 
-export default function Dynamic() {
+export default async function Dynamic() {
   console.log("Dynamic Page");
+  await new Promise((res) => setTimeout(res, 3000));
   return <div>Dynamic</div>;
 }
