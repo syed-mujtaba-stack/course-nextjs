31c04bd5420bc7b88bf89f5c3396d9dd0e7a9d24
basarat <basaratali@gmail.com>
2025-02-19 14:38:47 +1100

    yay

--- lessons/link-replace_example/src/app/page.tsx
+++ lessons/link-replace_example/src/app/page.tsx
@@ -1,10 +1,7 @@
-import Link from 'next/link';
-
 export default function Home() {
   return (
     <div>
       Hello Fam!
-      <Link href="/error">🚀 YOLO, Simulate an Error 💣</Link>
     </div>
   );
 }
