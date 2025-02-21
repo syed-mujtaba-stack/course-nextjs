066fb8e109c95fe1741c6d07271db73e1f100e43
basarat <basaratali@gmail.com>
2025-02-21 12:14:11 +1100

    better

--- lessons/image_img/src/app/page.tsx
+++ lessons/image_img/src/app/page.tsx
@@ -1,14 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 
-const responsiveImage = {
-  width: "100%",
-  height: "auto",
-};
-
 export default function Page() {
   return (
     <div>
-      <img style={responsiveImage} alt="dog" src="/images/dog.png" />
+      <img alt="dog" src="/images/dog.png" />
     </div>
   );
 }
