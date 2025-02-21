dde79e0f39eea42b52796318bcafc8e632eab25f
basarat <basaratali@gmail.com>
2025-02-21 18:29:24 +1100

    examples

--- lessons/image_static/src/app/page.tsx
+++ lessons/image_static/src/app/page.tsx
@@ -1,10 +1,32 @@
-import Image from "next/image";
-import dog from "../../public/images/dog.png";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <div>
-      <Image alt="dog" src={dog} />
+      <div>Pick a NextJS `Image` example</div>
+      <ul>
+        <li>
+          <Link href="/basic">Basic</Link>
+        </li>
+        <li>
+          <Link href="/scale">Automatic Resizing</Link>
+        </li>
+        <li>
+          <Link href="/placeholder">Placeholder Blur</Link>
+        </li>
+        <li>
+          <Link href="/quality">Quality</Link>
+        </li>
+        <li>
+          <Link href="/responsive">Responsive</Link>
+        </li>
+        <li>
+          <Link href="/loading">Lazy Loading vs Eager Loading</Link>
+        </li>
+        <li>
+          <Link href="/priority">Preloading (Priority)</Link>
+        </li>
+      </ul>
     </div>
   );
 }
