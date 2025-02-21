1735effabd5d53c5c239a793a6d72b610e1cd1a6
basarat <basaratali@gmail.com>
2025-02-21 12:43:03 +1100

    better

---
+++ lessons/image_static/src/app/responsive/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import dog from "../../../public/images/dog.png";
+
+export default function Page() {
+  return (
+    <div>
+      <Image alt="dog" src={dog} sizes="100vw" />
+    </div>
+  );
+}
