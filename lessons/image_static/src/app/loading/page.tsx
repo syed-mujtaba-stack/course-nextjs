6be07f4c81628d87509b97daa17391da7d8fcb65
basarat <basaratali@gmail.com>
2025-02-21 12:58:44 +1100

    loading

--- lessons/image_static/src/app/loading/page.tsx
+++ lessons/image_static/src/app/loading/page.tsx
@@ -8,11 +8,11 @@ import lizzard from "../../../public/images/lizzard.png";
 export default function Page() {
   return (
     <div>
-      <Image alt="cat" src={cat} />
-      <Image alt="dog" src={dog} />
-      <Image alt="chicken" src={chicken} />
-      <Image alt="rabbit" src={rabbit} />
-      <Image alt="lizzard" src={lizzard} />
+      <Image alt="cat" src={cat} loading="eager" />
+      <Image alt="dog" src={dog} loading="eager" />
+      <Image alt="chicken" src={chicken} loading="eager" />
+      <Image alt="rabbit" src={rabbit} loading="eager" />
+      <Image alt="lizzard" src={lizzard} loading="eager" />
     </div>
   );
 }
