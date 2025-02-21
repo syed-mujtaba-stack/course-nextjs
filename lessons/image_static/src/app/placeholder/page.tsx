bd586e923793a2e658d519b25659c152f1df4fbf
basarat <basaratali@gmail.com>
2025-02-21 14:40:25 +1100

    better

--- lessons/image_static/src/app/placeholder/page.tsx
+++ lessons/image_static/src/app/placeholder/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
+import cat from "../../../public/images/cat.png";
 import dog from "../../../public/images/dog.png";
+import chicken from "../../../public/images/chicken.png";
+import rabbit from "../../../public/images/rabbit.png";
+import lizzard from "../../../public/images/lizzard.png";
 
 export default function Page() {
   return (
     <div>
+      <Image alt="cat" src={cat} placeholder='blur' />
       <Image alt="dog" src={dog} placeholder='blur' />
+      <Image alt="chicken" src={chicken} placeholder='blur' />
+      <Image alt="rabbit" src={rabbit} placeholder='blur' />
+      <Image alt="lizzard" src={lizzard} placeholder='blur' />
     </div>
   );
 }
