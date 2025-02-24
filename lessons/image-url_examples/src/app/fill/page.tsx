19c57760adfff164b4ab93f3b814c950251ca6fb
basarat <basaratali@gmail.com>
2025-02-25 10:17:46 +1100

    better

--- lessons/image-url_examples/src/app/fill/page.tsx
+++ lessons/image-url_examples/src/app/fill/page.tsx
@@ -2,9 +2,14 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <div style={{ position: "relative", width: 480, height: 480 }}>
+    <div
+      style={{
+        position: "relative",
+        width: 480,
+        height: 480,
+      }}
+    >
       <Image fill alt="dog" src="/images/dog.png" />
-      <div>Image of a dog</div>
     </div>
   );
 }
--- lessons/image-url_examples/src/app/object-fit/page.tsx
+++ lessons/image-url_examples/src/app/object-fit/page.tsx
@@ -2,14 +2,38 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <div style={{ position: "relative", width: 480, height: 240 }}>
-      <Image
-        fill
-        style={{ objectFit: "contain" }}
-        alt="dog"
-        src="/images/dog.png"
-      />
+    <Column>
+      <div
+        style={{
+          position: "relative",
+          background: "gray",
+          width: 480,
+          height: 480,
+        }}
+      >
+        <Image
+          fill
+          style={{ objectFit: "contain" }}
+          alt="dog"
+          src="/images/dog.png"
+        />
+      </div>
       <div>Image of a dog</div>
-    </div>
+    </Column>
   );
 }
+
+const Column = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 16,
+      }}
+    >
+      {children}
+    </div>
+  );
+};
