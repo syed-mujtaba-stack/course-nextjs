d3934368d2eaeab2241e0f4f24b5381cb0454086
basarat <basaratali@gmail.com>
2025-02-25 11:53:12 +1100

    better

--- lessons/image-url_examples/src/app/remote/page.tsx
+++ lessons/image-url_examples/src/app/remote/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Page() {
   return (
     <div>
-      <Image alt="dog" src="https://booleanart.com/images/examples/dog.png" width={960} height={960} />
+      <Image
+        alt="dog"
+        src="https://booleanart.com/images/examples/dog.png"
+        width={960}
+        height={960}
+      />
       <div>Nice Doggy</div>
     </div>
   );
