1dee49598c1427398c6b1495701a6609799b3c0a
basarat <basaratali@gmail.com>
2025-03-04 14:48:18 +1100

    better

--- lessons/route-cookies/src/app/api/whoami/route.ts
+++ lessons/route-cookies/src/app/api/whoami/route.ts
@@ -2,5 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function GET(request: NextRequest) {
   const authCookie = request.cookies.get("auth");
-  return new NextResponse(authCookie ? authCookie.value : "Anonymous");
+  const whoami = authCookie ? authCookie.value : "Anonymous";
+  return new NextResponse(whoami);
 }
