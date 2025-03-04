63c3261991e7780cb418d73df93a91006c75e3e0
basarat <basaratali@gmail.com>
2025-03-04 14:05:03 +1100

    better

--- lessons/route-cookies/src/app/page.tsx
+++ lessons/route-cookies/src/app/page.tsx
@@ -1,25 +1,38 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Page() {
   const [authCookieValue, setAuthCookieValue] = useState("");
-  const makeSetRequest = async () => {
-    await fetch("/api/cookies/set");
-  };
+
   const makeGetRequest = async () => {
     const response = await fetch("/api/cookies/get");
     const data = await response.json();
     setAuthCookieValue(data.auth);
   };
 
+  const makeSetRequest = async () => {
+    await fetch("/api/cookies/set");
+    await makeGetRequest();
+  };
+
+  const makeDeleteRequest = async () => {
+    await fetch("/api/cookies/delete");
+    await makeGetRequest();
+  };
+
+  useEffect(() => {
+    makeGetRequest();
+  }, []);
+
   return (
     <>
-      <h1>Demo: NextRequest && NextResponse</h1>
-      <h2>Cookies</h2>
-      <button onClick={makeSetRequest}>Set Cookie `auth`</button>
-      <button onClick={makeGetRequest}>Read Cookie `auth`</button>
-      {authCookieValue && <p>`auth` Cookie Value:{authCookieValue}</p>}
+      <h1>Demo: Route Cookies</h1>
+      <div className='row'>
+        <button onClick={makeSetRequest}>Set Cookie `auth`</button>
+        <button onClick={makeDeleteRequest}>Delete Cookie `auth`</button>
+      </div>
+      <p>`auth` Cookie Value:{authCookieValue ?? "<COOKIE NOT SET>"}</p>
     </>
   );
 }
