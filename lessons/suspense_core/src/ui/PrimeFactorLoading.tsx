41d09c049beb07e146d587ea505c93c2cffa34bd
basarat <basaratali@gmail.com>
2025-03-06 12:16:34 +1100

    wip

--- lessons/suspense_core/src/ui/PrimeFactorLoading.tsx
+++ lessons/suspense_core/src/ui/PrimeFactorLoading.tsx
@@ -1,5 +1,5 @@
-import { commonClassName } from "./common";
+import styles from "./primeFactorStyles.module.css";
 
 export function PrimeFactorLoading() {
-  return <div className={`animate-pulse bg-gray-300 ${commonClassName}`} />;
+  return <div className={`animate-pulse bg-gray-300 ${styles.common}`} />;
 }
