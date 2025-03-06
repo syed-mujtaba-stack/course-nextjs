f29333a24dce9e9d4b5674ae85012fefb733dae5
basarat <basaratali@gmail.com>
2025-03-07 10:36:45 +1100

    toc

--- lessons/suspense-boundary/src/lib/TableOfContents.tsx
+++ lessons/suspense-boundary/src/lib/TableOfContents.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
 import { fetchTableOfContents } from "./api";
-import { Column } from './Layout';
+import { Column } from "./Layout";
 
 export async function TableOfContents() {
-  await fetchTableOfContents();
-  return <Column className={`w-96 bg-blue-300 flex flex-1`}>asdf</Column>;
+  const toc = await fetchTableOfContents();
+  return (
+    <Column className={`w-96 bg-gray-700`}>
+      {toc.map((item, i) => (
+        <Link
+          key={item.slug}
+          href={`https://booleanart.com/course/typescript/${item.slug}`}
+          target="_blank"
+          className="block p-2 text-gray-300 hover:text-gray-800 hover:bg-blue-400 transition-all"
+        >
+          {i + 1}. {item.title}
+        </Link>
+      ))}
+    </Column>
+  );
 }
