fdead4c25e6b4aeeea5ca054332e8f3e0f2dd365
basarat <basaratali@gmail.com>
2025-03-06 12:23:18 +1100

    better

--- lessons/suspense_core/src/ui/primeFactorAPI.ts
+++ lessons/suspense_core/src/ui/primeFactorAPI.ts
@@ -1,3 +1,8 @@
+/**
+ * Note: This is a mock API
+ * If you can solve this for real
+ * You can get a Fields Medal 🌹
+ */
 export async function primeFactorAPI(_value: number) {
   const factors = [6999999989, 7000000001] as const;
   if (_value !== factors[0] * factors[1]) {
