bcbe9256c540a872e3842d99db25e884d11782b4
basarat <basaratali@gmail.com>
2025-03-06 12:07:04 +1100

    better

--- lessons/suspense_core/src/ui/PrimeFactor.tsx
+++ lessons/suspense_core/src/ui/PrimeFactor.tsx
@@ -2,6 +2,10 @@ import { primeFactorAPI } from "./primeFactorAPI";
 import { commonClassName } from "./common";
 
 export async function PrimeFactor() {
-  const result = await primeFactorAPI(489538065520461000);
-  return <div className={`text-4xl bg-blue-300 ${commonClassName}`}>Result: {result}</div>;
+  const [factor1, factor2] = await primeFactorAPI(48999999930000000000);
+  return (
+    <div className={`text-2xl bg-blue-300 ${commonClassName}`}>
+      Prime Factors: {factor1} x {factor2}
+    </div>
+  );
 }
--- lessons/suspense_core/src/ui/primeFactorAPI.ts
+++ lessons/suspense_core/src/ui/primeFactorAPI.ts
@@ -1,4 +1,8 @@
-export async function primeFactorAPI(value: number) {
+export async function primeFactorAPI(_value: number) {
+  const factors = [6999999989, 7000000001] as const;
+  if (_value !== factors[0] * factors[1]) {
+    throw new Error("Invalid value");
+  }
   await new Promise((resolve) => setTimeout(resolve, 5000));
-  return Math.sqrt(value);
+  return factors;
 }
