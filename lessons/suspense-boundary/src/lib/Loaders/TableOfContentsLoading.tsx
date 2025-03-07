645029f80a1a33b12010f16382847a52cc232768
basarat <basaratali@gmail.com>
2025-03-07 11:42:04 +1100

    fixed

--- lessons/suspense-boundary/src/lib/Loaders/TableOfContentsLoading.tsx
+++ lessons/suspense-boundary/src/lib/Loaders/TableOfContentsLoading.tsx
@@ -3,7 +3,7 @@ import { Spinner } from "./Spinner";
 export function TableOfContentsLoading() {
   return (
     <div
-      className={`w-[569px] bg-gray-700 h-full animate-pulse justify-center items-center flex`}
+      className={`w-[569px] bg-gray-700 animate-pulse justify-center items-center flex`}
     >
       <Spinner />
     </div>
