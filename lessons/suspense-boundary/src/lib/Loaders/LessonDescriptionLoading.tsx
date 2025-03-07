e44abe42bf70456338ede8d3c3696e19e4d6b1d9
basarat <basaratali@gmail.com>
2025-03-07 12:11:03 +1100

    better

--- lessons/suspense-boundary/src/lib/Loaders/LessonDescriptionLoading.tsx
+++ lessons/suspense-boundary/src/lib/Loaders/LessonDescriptionLoading.tsx
@@ -1,7 +1,7 @@
 export const LessonDescriptionLoading = () => {
   return (
     <div
-      className={`bg-gray-600 animate-pulse p-4 flex flex-1 text-white text-2xl`}
+      className={`bg-gray-800 animate-pulse p-4 flex flex-1 text-white text-2xl`}
     >
       Loading Lesson Description...
     </div>
