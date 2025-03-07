4a8b2a05dfc6db9592da4714184d3465d4fa0bcf
basarat <basaratali@gmail.com>
2025-03-07 14:37:56 +1100

    better

--- lessons/suspense-boundary/src/lib/Loaders/LessonLoading.tsx
+++ lessons/suspense-boundary/src/lib/Loaders/LessonLoading.tsx
@@ -3,7 +3,7 @@ export const LessonLoading = () => {
     <div
       className={`bg-gray-300 animate-pulse justify-center flex flex-1`}
     >
-      <h2 className="text-2xl font-bold text-center p-8">Loading Lesson...</h2>
+      <h2 className="text-2xl font-bold text-center p-8">Loading Video && Description...</h2>
     </div>
   );
 };
