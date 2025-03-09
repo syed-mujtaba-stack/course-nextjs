5d6a63f415f381b01f6878e5616994c35be6100b
Basarat Ali Syed <basaratali@gmail.com>
2025-03-09 13:35:36 +1100

    Update PrimeFactors.tsx

--- lessons/use-promise/src/ui/PrimeFactors.tsx
+++ lessons/use-promise/src/ui/PrimeFactors.tsx
@@ -1,13 +1,28 @@
-'use client';
+"use client";
 
 import styles from "./primeFactorsStyles.module.css";
 import { primeFactorsAPI } from "./primeFactorsAPI";
+import { useEffect, useState } from "react";
+import { PrimeFactorLoading } from "./PrimeFactorsLoading";
 
 export function PrimeFactors({ input }: { input: number }) {
-  primeFactorsAPI(input);
+  const [factors, factorsSet] = useState<[number, number] | null>(null);
+
+  useEffect(() => {
+    primeFactorsAPI(input).then(([factor1, factor2]) => {
+      factorsSet([factor1, factor2]);
+    });
+  }, [input]);
+
+  if (factors === null) {
+    return <PrimeFactorLoading />;
+  }
+
+  const [factor1, factor2] = factors;
+
   return (
     <div className={`bg-blue-300 ${styles.component}`}>
-      
+      Prime Factors: {factor1} x {factor2}
     </div>
   );
 }
