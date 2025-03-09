409535404ca978ea8efe12e0de8f5fe2d7ac29a4
Basarat Ali Syed <basaratali@gmail.com>
2025-03-09 13:41:17 +1100

    better

--- lessons/use-promise/src/app/layout.tsx
+++ lessons/use-promise/src/app/layout.tsx
@@ -13,7 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className='foundation'>{children}</div>
+      </body>
     </html>
   );
 }
