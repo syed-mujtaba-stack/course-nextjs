e40b0b617cb80a3b6f2b4510f0f95479bdf6d5d6
basarat <basaratali@gmail.com>
2025-03-12 13:19:44 +1100

    formta

--- lessons/use-promise_with/src/app/page.tsx
+++ lessons/use-promise_with/src/app/page.tsx
@@ -6,11 +6,12 @@ import { Suspense } from "react";
 export default function Home() {
   const input = 6999999989 * 7000000001;
   const promise = primeFactorsAPI(input);
+
   return (
     <>
       <h1 className="text-3xl">Prime Factors for {input}</h1>
       <Suspense fallback={<PrimeFactorLoading />}>
-        <PrimeFactors promise={promise} />
+        <PrimeFactors promise={promise}/>
       </Suspense>
     </>
   );
