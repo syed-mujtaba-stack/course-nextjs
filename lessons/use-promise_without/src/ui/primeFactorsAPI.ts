5de5483f78f68ee80c8b30e8885ed728dbcb8d22
basarat <basaratali@gmail.com>
2025-03-12 12:55:15 +1100

    better

--- lessons/use-promise_without/src/ui/primeFactorsAPI.ts
+++ lessons/use-promise_without/src/ui/primeFactorsAPI.ts
@@ -3,8 +3,10 @@
  * If you can solve this for real
  * You can get a Fields Medal 🌹
  */
-export async function primeFactorsAPI(input: number) {
-  const factors = [6999999989, 7000000001] as const;
+export async function primeFactorsAPI(
+  input: number
+): Promise<[number, number]> {
+  const factors: [number, number] = [6999999989, 7000000001];
   if (input !== factors[0] * factors[1]) {
     throw new Error("Invalid value");
   }
