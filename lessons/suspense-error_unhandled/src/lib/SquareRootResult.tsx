6094f0c0c75eb812841003655ab4d1ebf719a1b4
basarat <basaratali@gmail.com>
2025-03-12 11:23:36 +1100

    wip

--- lessons/suspense-error_unhandled/src/lib/SquareRootResult.tsx
+++ lessons/suspense-error_unhandled/src/lib/SquareRootResult.tsx
@@ -2,6 +2,9 @@ import { use } from 'react';
 import { sqrt } from './api';
 
 export const SquareRootResult = ({ value }: { value: number }) => {
+  console.log('call start');
   const result = use(sqrt(value));
+  console.log('call end');
+  
   return <div>{result}</div>;
 }
--- lessons/suspense-error_unhandled/src/lib/api.ts
+++ lessons/suspense-error_unhandled/src/lib/api.ts
@@ -1,11 +1,15 @@
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export async function sqrt(n: number) {
-  console.log('sqrt:input:', n);
+  console.log(":input:", n);
   await delay(1000); // simulate slow network
+  return sqrtCore(n);
+}
+
+export function sqrtCore(n: number) {
   if (n < 0) {
-    throw new Error('Cannot sqrt negative number');
+    throw new Error("Cannot sqrt negative number");
   }
-  const result = Math.sqrt(n).toFixed(2)
+  const result = Math.sqrt(n).toFixed(2);
   return result;
 }
