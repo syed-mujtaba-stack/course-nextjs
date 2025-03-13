358ff9a2393d0ac14cac0ba7a2f8a08c9327d424
basarat <basaratali@gmail.com>
2025-03-13 14:20:57 +1100

    fit screen

--- lessons/server-functions_core/src/lib/functions.ts
+++ lessons/server-functions_core/src/lib/functions.ts
@@ -6,14 +6,12 @@ export async function getLikes() {
   const db = await getDb();
   return db.data.likes;
 }
-
 export async function incrementLikes() {
   const db = await getDb();
   db.data.likes++;
   await db.write();
   return db.data.likes;
 }
-
 export async function resetLikes() {
   const db = await getDb();
   db.data.likes = 0;
