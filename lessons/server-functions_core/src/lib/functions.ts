bf13f7bc5cffb4fbf89e39103b5be80bab20c62a
basarat <basaratali@gmail.com>
2025-03-13 12:15:31 +1100

    another

---
+++ lessons/server-functions_core/src/lib/functions.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import { getDb } from "./db";
+
+export async function getLikes() {
+  const db = await getDb();
+  return db.data.likes;
+}
+
+export async function incrementLikes() {
+  const db = await getDb();
+  db.data.likes++;
+  await db.write();
+  return db.data.likes;
+}
