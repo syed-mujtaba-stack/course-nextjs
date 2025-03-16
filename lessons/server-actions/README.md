66859a8e9f5dc43c2c053cc617d4c2c63f0f84e1
Basarat Ali Syed <basaratali@gmail.com>
2025-03-16 16:41:37 +1100

    useless

--- lessons/server-actions/README.md
+++
@@ -1,3 +0,0 @@
-# BooleanArt NextJS Sample
-
-This is an example project for the [NextJS course on BooleanArt](https://www.booleanart.com/course/nextjs).
--- lessons/server-actions/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { SubmitButton } from "@/lib/Button";
-
-export default function Home() {
-  return <SubmitButton>Login</SubmitButton>;
-}
