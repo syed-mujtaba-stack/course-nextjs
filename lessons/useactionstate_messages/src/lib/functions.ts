5283e0a624a1a00ffdb496d17c76bd68b998517a
basarat <basaratali@gmail.com>
2025-03-17 20:34:46 +1100

    better

--- lessons/useactionstate_messages/src/lib/functions.ts
+++ lessons/useactionstate_messages/src/lib/functions.ts
@@ -6,7 +6,7 @@ export type AddToCartResponse = {
 };
 
 export async function addToCart(
-  _prevState: unknown,
+  _state: unknown,
   formData: FormData
 ): Promise<AddToCartResponse> {
   await new Promise((resolve) => setTimeout(resolve, 1000));
