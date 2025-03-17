4680cbb4e228a70c9714d6bcea1aa7ffa88d8fc7
basarat <basaratali@gmail.com>
2025-03-17 15:16:01 +1100

    yay

--- lessons/useactionstate_sync/src/lib/functions.ts
+++ lessons/useactionstate_sync/src/lib/functions.ts
@@ -8,9 +8,7 @@ export async function initial(): Promise<ServerState> {
   return { likes: 0 };
 }
 
-export async function update(prevState: {
-  likes: number;
-}): Promise<ServerState> {
+export async function update(prevState: ServerState): Promise<ServerState> {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   return { likes: prevState.likes + 1 };
 }
