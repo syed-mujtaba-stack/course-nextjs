28a88a65f4cd10bba30791d6977eb26d7cc0ed48
basarat <basaratali@gmail.com>
2025-03-18 16:15:28 +1100

    better

--- lessons/imageresponse_core/src/app/api/route.tsx
+++ lessons/imageresponse_core/src/app/api/route.tsx
@@ -2,7 +2,25 @@ import { ImageResponse } from "next/og";
 
 export async function GET() {
   return new ImageResponse(
-    <div style={{ fontSize: "24px" }}>Simple Image</div>,
+    (
+      <div
+        style={{
+          /** Take up all the space from the parent */
+          height: "100%",
+          width: "100%",
+
+          /** Setup a column layout */
+          display: "flex",
+          flexDirection: "column",
+
+          /** Center the children */
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div style={{ fontSize: "36px" }}>Sample Image</div>
+      </div>
+    ),
     {
       width: 300,
       height: 300,
