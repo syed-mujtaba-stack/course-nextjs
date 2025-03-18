27b461e2fe9d07b5efbfc2bcd8043403d5df3e99
basarat <basaratali@gmail.com>
2025-03-18 16:48:50 +1100

    params

---
+++ lessons/imageresponse_params/src/app/api/route.tsx
@@ -0,0 +1,31 @@
+import {
+  ImageResponseCenteredText,
+  ImageResponseFullBackgroundImg,
+  ImageResponseRoot,
+} from "@/lib/ImageResponseComponents";
+import { ImageResponse } from "next/og";
+import { fontGagalin, getFontGagalin } from "@/lib/ImageResponseFonts";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const message =
+    new URL(req.url).searchParams.get("message") ?? "NO MESSAGE";
+
+  return new ImageResponse(
+    (
+      <ImageResponseRoot>
+        <ImageResponseFullBackgroundImg src="http://localhost:3000/images/background.png" />
+        <ImageResponseCenteredText>
+          <div style={{ fontSize: "69px", fontFamily: fontGagalin }}>
+            {message}
+          </div>
+        </ImageResponseCenteredText>
+      </ImageResponseRoot>
+    ),
+    {
+      width: 1080,
+      height: 1080,
+      fonts: [await getFontGagalin()],
+    }
+  );
+}
