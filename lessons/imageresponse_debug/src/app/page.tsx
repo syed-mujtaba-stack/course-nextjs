50a19c21ce346e5f681bc720f984b788bb6e0d9c
basarat <basaratali@gmail.com>
2025-03-18 16:51:35 +1100

    debug

---
+++ lessons/imageresponse_debug/src/app/page.tsx
@@ -0,0 +1,19 @@
+export default function Page() {
+  return (
+    <div className="root">
+      <div>
+        The following <b>Image</b> is generated with <code>ImageResponse</code>
+      </div>
+      <img
+        src="/api?message=HELLO%20WORLD"
+        alt="image"
+        style={{
+          width: "300px",
+          height: "300px",
+          boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
+          borderRadius: "5px",
+        }}
+      />
+    </div>
+  );
+}
