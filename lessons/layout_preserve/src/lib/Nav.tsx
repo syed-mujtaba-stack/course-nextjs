67281e48b270303009707e32229f6bde1b002cf7
basarat <basaratali@gmail.com>
2025-03-25 12:13:26 +1100

    wip

--- lessons/layout_preserve/src/lib/Nav.tsx
+++ lessons/layout_preserve/src/lib/Nav.tsx
@@ -1,16 +1,27 @@
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
+import styles from "./Nav.module.css";
 
 export const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav>
-      <ul>
-        <li>
-          <Link href="/dashboard">Home</Link>
-        </li>
-        <li>
-          <Link href="/dashboard/subscriptions">Subscriptions</Link>
-        </li>
-      </ul>
+      <button onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? "Close Menu" : "Open Menu"}
+      </button>
+      {isOpen && (
+        <ul>
+          <li>
+            <Link href="/dashboard">Home</Link>
+          </li>
+          <li>
+            <Link href="/dashboard/subscriptions">Subscriptions</Link>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 };
