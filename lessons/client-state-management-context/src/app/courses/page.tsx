f82bef904fdf5defd860a3d40f8b3a6517c8d5a8
basarat <basaratali@gmail.com>
2025-03-26 18:35:35 +1100

    wip

---
+++ lessons/client-state-management-context/src/app/courses/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Client State Management with Context</h1>
+      <p>Checkout the courses</p>
+      <p>View your cart</p>
+    </div>
+  );
+}
