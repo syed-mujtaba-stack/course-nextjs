0452ed193e2cebf01985cf0c8d2345e005015540
basarat <basaratali@gmail.com>
2025-03-26 18:35:00 +1100

    better

--- lessons/client-state-management-context/src/app/page.tsx
+++ lessons/client-state-management-context/src/app/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import { Button } from "@/lib/Button";
-
 export default function Page() {
-  return <Button onClick={() => alert("Stop Clicking Me!")}>Click Me!</Button>;
+  return (
+    <div>
+      <h1>Client State Management with Context</h1>
+      <p>Checkout the courses</p>
+      <p>View your cart</p>
+    </div>
+  );
 }
