dd6ba9c4a075f66d17c34ac9e0b7dbfef8cc93b7
basarat <basaratali@gmail.com>
2025-03-27 08:28:56 +1100

    better

--- lessons/client-state-management-context/src/app/page.tsx
+++ lessons/client-state-management-context/src/app/page.tsx
@@ -4,6 +4,9 @@ export default async function Page() {
   return (
     <div className={styles.root}>
       <h1 className={styles.heading}>Client State Management</h1>
+      <p className={styles.subheading}>
+        Visit the different pages by using the links in the header 🫰🏻
+      </p>
     </div>
   );
 }
