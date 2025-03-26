472260cdb67f9b5392bfb29c9f69b19e217e56f0
Basarat Ali Syed <basaratali@gmail.com>
2025-03-26 20:28:07 +1100

    better

--- lessons/client-state-management-context/src/app/page.tsx
+++ lessons/client-state-management-context/src/app/page.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
+import styles from "./page.module.css";
+
 export default async function Page() {
   return (
-    <div>
-      <h1>Client State Management with Context</h1>
-      <p>Checkout the courses</p>
-      <p>View your cart</p>
+    <div className={styles.root}>
+      <h1 className={styles.heading}>Client State Management with Context</h1>
+      <Link href='/courses'>Courses Page</Link>
+      <Link href='/courses'>Checkout Page</Link>
     </div>
   );
 }
