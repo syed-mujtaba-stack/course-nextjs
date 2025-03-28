18c56cf1b0b7bd9223c61f72d7ffef4a4335d037
basarat <basaratali@gmail.com>
2025-03-28 18:06:20 +1100

    comment

--- lessons/detecting-runtime_global/src/app/hydrate/page.tsx
+++ lessons/detecting-runtime_global/src/app/hydrate/page.tsx
@@ -3,5 +3,12 @@
 import DetectEnvironment from "@/lib/DetectEnvironment";
 
 export default function Page() {
+  /** 
+   * This will result in a runtime hydration error
+   * Reason: 
+   * - client components render on server and hydrate on client
+   * - the output of both should match
+   * - this is not the case for <DetectEnvironment/>
+   */
   return <DetectEnvironment />;
 }
