96046896d0285e793bbf67dc5ae93fa49aff495c
Basarat Ali Syed <basaratali@gmail.com>
2025-03-28 13:35:02 +1100

    wip

--- lessons/detecting-runtime_global/src/app/page.tsx
+++ lessons/detecting-runtime_global/src/app/page.tsx
@@ -1,11 +1,26 @@
-import styles from './page.module.css'
-import Link from 'next/link';
+import styles from "./page.module.css";
 
 export default function Page() {
-  return (<div className={styles.root}>
-    <h1 className={styles.title}>Detect Runtime Environment</h1>
-    <Link href="/server">Render on Server Only</Link>
-    <Link href="/client">Render on Client Only</Link>
-    <Link href="/hydrate">Render on Server and Hydrate on Client</Link>
-  </div>)
+  return (
+    <div className={styles.root}>
+      <h1 className={styles.title}>Detect Runtime Environment</h1>
+      <ExampleLink href="/server">Render on Server Only</ExampleLink>
+      <ExampleLink href="/client">Render on Client Only</ExampleLink>
+      <ExampleLink href="/hydrate">
+        Render on Server and Hydrate on Client
+      </ExampleLink>
+    </div>
+  );
 }
+
+import Link from "next/link";
+const ExampleLink = (props: React.PropsWithChildren<{ href: string }>) => {
+  return (
+    <div>
+      👉🏻{" "}
+      <Link className={styles.link} href={props.href}>
+        {props.children}
+      </Link>
+    </div>
+  );
+};
