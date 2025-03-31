98231f4bc3078e9e0c1835ebdf8481e26c93f1a3
basarat <basaratali@gmail.com>
2025-04-01 06:54:47 +1100

    format

--- lessons/incremental-static-regeneration_fetch/src/app/ssg/page.tsx
+++ lessons/incremental-static-regeneration_fetch/src/app/ssg/page.tsx
@@ -2,7 +2,7 @@ export default async function Page() {
   console.log("Rendering SSG page...");
 
   const content = await fetch(process.env.NEXT_PUBLIC_API_URL + "/content");
-  
+
   const { message } = await content.json();
   return <div>{message}</div>;
 }
