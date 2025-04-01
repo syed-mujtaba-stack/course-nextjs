27096f51641f33b8b81ddbededbb21df03406f13
basarat <basaratali@gmail.com>
2025-04-02 10:03:59 +1100

    release

--- lessons/sitemap_core/src/app/page.tsx
+++ lessons/sitemap_core/src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Page() {
   return (
     <div className={styles.root}>
-      <h1 className={styles.title}>Single Page, Multiple Static Routes</h1>
+      <h1 className={styles.title}>Generate a sitemap.xml listing the different products</h1>
       <ExampleLink href="/product/apple">Apple</ExampleLink>
       <ExampleLink href="/product/banana">Banana</ExampleLink>
       <ExampleLink href="/product/orange">Orange</ExampleLink>
