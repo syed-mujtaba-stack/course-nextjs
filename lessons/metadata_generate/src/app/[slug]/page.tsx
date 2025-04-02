be2246e14e9c44c9d68cb2eaaef00a249a1777e1
Basarat Ali Syed <basaratali@gmail.com>
2025-04-02 19:44:25 +1100

    better

--- lessons/metadata_generate/src/app/[slug]/page.tsx
+++ lessons/metadata_generate/src/app/[slug]/page.tsx
@@ -1,8 +1,24 @@
-import { fetchPageContent } from '@/api/cms';
-import { SimpleContent } from '@/lib/SimpleContent';
+import { fetchPageContent } from "@/api/cms";
+import { SimpleContent } from "@/lib/SimpleContent";
+import { Metadata } from "next";
 
-export default async function Page({params}: {
-  params: Promise<{ slug: string }>
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const { slug } = await params;
+  const content = await fetchPageContent(slug);
+  return {
+    title: content.title,
+    description: content.description,
+  };
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
 }) {
   const { slug } = await params;
   const content = await fetchPageContent(slug);
