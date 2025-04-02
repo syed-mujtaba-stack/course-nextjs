453d54419e5a2cbbf9d7f52477d6aae91a187762
basarat <basaratali@gmail.com>
2025-04-03 09:10:08 +1100

    better

--- lessons/metadata_override/src/app/dashboard/layout.tsx
+++ lessons/metadata_override/src/app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import type { Metadata } from "next";
+
 export const metadata = {
   title: "Dashboard",
   description: "Manage your user settings and preferences",
--- lessons/metadata_override/src/app/dashboard/subscription/page.tsx
+++ lessons/metadata_override/src/app/dashboard/subscription/page.tsx
@@ -1,14 +1,9 @@
 import { SimpleContentProps, SimpleContent } from "@/lib/SimpleContent";
-import { Metadata } from "next";
 
 const content: SimpleContentProps = {
   title: "Subscription Settings",
   icon: "💳",
   description: "Manage your subscription settings and preferences here. 📅",
-};
-
-export const metadata: Metadata = {
-  title: content.title + " | Dashboard",
 };
 
 export default async function Page() {
